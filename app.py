b2dc7be1ac78a27cd778a463f6415d1247497582
Bill Dailey <billcd@me.com>
2023-12-10 02:31:25 -0500

    change to template

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    body = "This is running from a template!"
+    return render_template('default.html', body=body)
 
 
 if __name__ == '__main__':
